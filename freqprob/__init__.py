a6cd9f879d99a3a4a2412232e44f1035f2604166
Tiago Tresoldi <tresoldi@gmail.com>
2023-02-18 18:11:15 +0100

    Add Witten-Bell scorer

--- freqprob/__init__.py
+++ freqprob/__init__.py
@@ -9,7 +9,7 @@ __email__ = "tiago.tresoldi@lingfil.uu.se"
 
 
 # Import from local modules
-from .freqprob import ELE, MLE, Laplace, Lidstone, Random, Uniform
+from .freqprob import ELE, MLE, Laplace, Lidstone, Random, Uniform, WittenBell
 
 # Build the namespace
-__all__ = ["Uniform", "Random", "MLE", "Lidstone", "Laplace", "ELE"]
+__all__ = ["Uniform", "Random", "MLE", "Lidstone", "Laplace", "ELE", "WittenBell"]
